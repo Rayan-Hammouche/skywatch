05d2ecb4e226fff29aafea158f97252a31a55b52
Yanis <166356311+Rayan-Hammouche@users.noreply.github.com>
2024-08-06 11:53:26 +0100

    Update api.js

---
+++ assets/js/api.js
@@ -0,0 +1,10 @@
+/**
+ * @license MIT
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @copyright darkyanis 2024 All rights reserved
+ * @author darkyanis <yanisrayanhammouche@gmail.com>
+ */
+
+'use strict';
+
+const api_key = "9967fafade47dfdb08616e83012b1522"
